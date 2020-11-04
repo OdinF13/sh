70902aced66774bf21cf3e6ade1f58bdbedbe55c
Odin <36133617+OdinF13@users.noreply.github.com>
2020-11-04 16:16:07 +0400

    Create secdel.sh

---
+++ secdel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: "$0 "[file]..." >&2
+  exit 1
+fi
+
+for file in "$@"; do
+
+  if [[ ! -f "$file" ]]; then
+    echo "WARNING: \"$file\" is not a file"
+    continue
+  fi
+  
+  shred -uzv -n 5 "$file"
+
+done
